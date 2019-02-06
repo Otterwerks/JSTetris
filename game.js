0b4965c16384d6f24c5c155ccf11b060e7e088b0
Otterwerks <44848311+Otterwerks@users.noreply.github.com>
2019-02-06 07:56:03 -0800

    Leaderboard update

--- game.js
+++ game.js
@@ -551,7 +551,7 @@ function moveGamePiece(direction) {
 
 function getLeaderboard() {
     let xhr = new XMLHttpRequest();
-    xhr.open("POST", "http://otterwerks.net:2222/leaderboard", true);
+    xhr.open("POST", "otterwerks.net:2222/leaderboard", true);
     xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
     xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
     xhr.responseType = 'text';

0fb9b974f57494b172194772c6a912386dad53b7
Otterwerks <44848311+Otterwerks@users.noreply.github.com>
2019-02-07 18:25:05 -0800

    Debugging shadow piece

--- game.js
+++ game.js
@@ -365,10 +365,10 @@ function gamePieceShadow() {
             shadowPiece.updateTemplate();
         }
         else if (fallenPieces.length > 0) {
-            for (let j = fallenPieces.length; j > 0; j--) {
-                if (shadowPiece.template[i][0] == fallenPieces[j - 1][0] &&
-                    (shadowPiece.template[i][1] + baseUnitSize) > fallenPieces[j - 1][1]) {
-                        shadowPiece.yPosition = shadowPiece.yPosition - (shadowPiece.template[i][1] - fallenPieces[j - 1][1]) - baseUnitSize;
+            for (let j = 0; j < fallenPieces.length; j++) {
+                if (shadowPiece.template[i][0] == fallenPieces[j][0] &&
+                    (shadowPiece.template[i][1] + baseUnitSize) > fallenPieces[j][1]) {
+                        shadowPiece.yPosition = shadowPiece.yPosition - (shadowPiece.template[i][1] - fallenPieces[j][1]) - baseUnitSize;
                         shadowPiece.updateTemplate();
                 }
             }

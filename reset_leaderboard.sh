a0807a2d15be3be8bd92551dba7be0cbd506b28c
Otterwerks <44848311+Otterwerks@users.noreply.github.com>
2019-02-26 11:00:28 -0800

    Create reset_leaderboard.sh

---
+++ reset_leaderboard.sh
@@ -0,0 +1,8 @@
+mongo JSTetris_server
+db.scores.drop()
+db.scores.insert({name: "Tetro Game", score: 350)}
+db.scores.insert({name: "Tetro Game", score: 300)}
+db.scores.insert({name: "Tetro Game", score: 250)}
+db.scores.insert({name: "Tetro Game", score: 200)}
+db.scores.insert({name: "Tetro Game", score: 150)}
+exit
